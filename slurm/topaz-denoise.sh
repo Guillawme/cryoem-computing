b8e14339b185aa53b25e21ab62a86d19a1b63acc
Guillaume Gaullier <guillaume@gaullier.org>
2022-11-03 15:16:58 +0100

    Updated slurm scripts

--- slurm/topaz-denoise.sh
+++ slurm/topaz-denoise.sh
@@ -14,19 +14,39 @@
 DATA_PATH=/path/to/input/images/*.mrc
 OUT_PATH=/path/to/output/folder
 
+# MODEL can be one of:
+# unet, unet-small, fcnn, affine, unet-v0.2.1
+# or a full path to a .sav file from a custom training run
+MODEL=unet 
+
+# OUT_FORMAT can be one of:
+# mrc, png, tiff, jpg
+OUT_FORMAT=mrc 
+
+# Only change PATCH_SIZE and PATCH_PADDING if you know you need it
+# The default values are good in most cases
+PATCH_SIZE=1536
+PATCH_PADDING=384
+
 # Remember to check which modulefile version is the default one
 # You might want to specify a version explicitely (e.g. module/1.2.0)
 module purge
 module load topaz
 
+if [[ ! -d ${OUT_PATH} ]]; then                                                 
+        mkdir -p ${OUT_PATH}                                                    
+fi                                                                              
+                                                                                
+# Option '--device -2' will use all GPUs requested with --gres=gpu:
+# Option '-j -1' will use all cores requested with --cpus-per-task
 srun topaz denoise \
 	$DATA_PATH \
-	--model unet \
-	--device -2 \ # will use all GPUs requested with --gres=gpu:
-	-j -1 \ # will use all cores requested with --cpus-per-task
-	--format mrc \
-	--patch-size 1536 \
-	--patch-padding 384 \
+	--model $MODEL \
+	--device -2 \ 
+	-j -1 \ 
+	--format $OUT_FORMAT \
+	--patch-size $PATCH_SIZE \
+	--patch-padding $PATCH_PADDING \
 	--normalize \
 	--output $OUT_PATH
 
--- slurm/topaz-denoise3d.sh
+++ slurm/topaz-denoise3d.sh
@@ -14,18 +14,38 @@
 DATA_PATH=/path/to/input/images/*.mrc
 OUT_PATH=/path/to/output/folder
 
+# MODEL can be one of:
+# unet-3d-10a, unet-3d-20a
+# or a full path to a .sav file from a custom training run
+MODEL=unet-3d-10a
+
+# Standard deviation of Gaussian filter applied after denoising
+# Default 0
+GAUSSIAN_FILTER=0
+
+# Only change PATCH_SIZE and PATCH_PADDING if you know you need it              
+# The default values are good in most cases                                     
+PATCH_SIZE=96
+PATCH_PADDING=48
+
 # Remember to check which modulefile version is the default one
 # You might want to specify a version explicitely (e.g. module/1.2.0)
 module purge
 module load topaz
 
+if [[ ! -d ${OUT_PATH} ]]; then
+	mkdir -p ${OUT_PATH}
+fi
+
+# Option '--device -2' will use all GPUs requested with --gres=gpu:
+# Option '-j -1' will use all cores requested with --cpus-per-task
 srun topaz denoise3d \
 	$DATA_PATH \
-	--model unet-3d-10a \
-	--device -2 \ # will use all GPUs requested with --gres=gpu:
-	-j -1 \ # will use all cores requested with --cpus-per-task
-	--gaussian 0 \
-	--patch-size 96 \
-	--patch-padding 48 \
+	--model $MODEL \
+	--device -2 \ 
+	-j -1 \ 
+	--gaussian $GAUSSIAN_FILTER \
+	--patch-size $PATCH_SIZE \
+	--patch-padding $PATCH_PADDING \
 	--output $OUT_PATH
 
