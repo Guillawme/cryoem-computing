f6a2596f4791565c8ad13734650d4b40e596748d
Guillaume Gaullier <guillaume@gaullier.org>
2025-10-16 10:49:51 +0200

    Update slurm scripts on ang-kemi

--- ang-kemi/slurm/phenix-molprobity.sh
+++ ang-kemi/slurm/phenix-molprobity.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=phenix-validation-cryoem
+#SBATCH --job-name=phenix-molprobity
 #SBATCH --partition=main
 #SBATCH --gres=gpu:0
 #SBATCH --ntasks=1
@@ -22,23 +22,15 @@ MODELS=${BASE_DIR}/models
 VALIDATION=${BASE_DIR}/validation
 
 # Inputs
-FULL_MAP=${MAPS}/cryosparc_P306_J130_011_volume_map.mrc
-HALF_MAPS=${MAPS}/cryosparc_P306_J130_011_volume_map_half_?.mrc
 MODEL=${MODELS}/model.cif
-PARAMS=${VALIDATION}/params.eff
-RESOLUTION=3.0
 
 # Remember to check which modulefile version is the default one
 # You might want to specify a version explicitely (e.g. module/1.2.0)
 module purge
 module load phenix/1.21.2-5419
 
-srun phenix.validation_cryoem \
+srun phenix.molprobity \
 	$MODEL \
-	$FULL_MAP \
-	$HALF_MAPS \
-	resolution=$RESOLUTION \
-	$PARAMS
 
 # Send notification upon job completion
 source /opt/slurm/slurm-completion.sh
